41e8c2e24e9d683af23aed9b57b2f433e0dd9b86
vackosar <vackosar@gmail.com>
2017-08-20 21:57:26 +0200

    anki backup

--- linux/cron/anki-backup.sh
+++ linux/cron/anki-backup.sh
@@ -1,1 +1,1 @@
-0 * * * * cp "$(ls -1t "$HOME/.local/share/Anki2/User 1/backups" |head -n 1)" "$HOME/Dropbox/bkp/rsr/anki.apkg"
+0 * * * * rsync -auv "$(ls -1t "$HOME/.local/share/Anki2/User 1/backups" |head -n 1)" "$HOME/Dropbox/bkp/rsr/anki.apkg"
